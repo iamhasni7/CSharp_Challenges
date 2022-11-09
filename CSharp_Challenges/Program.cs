6ad7d466975e80e2c906877ad30a78f0728c2078
Muhammad Hussnain Fareed <hasni.rock@yahoo.com>
2022-11-09 16:53:35 +0500

    Three Challenges related to Arrays completed

---
+++ CSharp_Challenges/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Challenges
+{
+    //Challenge 01
+    //Create a function that takes an array of numbers and returns the smallest number in the set.
+    /*internal class Program
+    {
+        public static int[] Arr(int[] arr)
+        {
+            Array.Sort(arr);
+            Console.WriteLine(arr[0]);
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+            Arr(new int[] { 34,25, 5, 17, 59 });
+            Console.ReadLine();
+        }
+    }*/
+
+    //Challenge 02
+    //Create a function that takes an array of numbers and returns the largest number in the set.
+    /*internal class Program
+    {
+        public static int[] Arr(int[] arr)
+        {
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            Console.WriteLine(arr[0]);
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+            Arr(new int[] {4, 8, 9, 1, 3});
+            Console.ReadLine();
+        }
+    }*/
+
+    //Challenge 03
+    //Create a function that finds the index of a given item.
+    /*internal class Program
+    {
+        static int[] Arr(int[] arr, int a)
+        {
+            int index = Array.IndexOf(arr, a);
+            Console.WriteLine(index);
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+            Arr(new int[] { 9, 8, 3 }, 3);
+            Console.ReadLine();
+        }
+    }*/
+
+    //Challenge 04
+    //
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
